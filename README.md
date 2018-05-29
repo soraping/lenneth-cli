4ed873e0132cce5cf081477e74d2d03c7d5c4ca7
caoshiping <caoshiping@qianmi.com>
2018-05-29 15:42:41 +0800

    新模版

--- README.md
+++ README.md
@@ -1,2 +1,16 @@
 # lenneth-cli
+
 lenneth 脚手架
+
+> 安装
+
+```bash
+# 全局安装脚手架
+npm install -g lenneth-cli
+
+# 查看安装版本
+lenneth -v
+
+# 创建工程目录
+lenneth init yourApp
+```
--- src/init.ts
+++ src/init.ts
@@ -1,9 +1,14 @@
 /**
  * 初始化类
  */
-import * as download from "git-downloader";
-import * as ora from "ora";
-import * as inquirer from "inquirer";
+import * as path from "path";
+import * as fs from "fs";
+import * as Download from "git-downloader";
+import * as Ora from "ora";
+import * as Inquirer from "inquirer";
+import * as Minimatch from "minimatch";
+import * as Metalsmith from "metalsmith";
+import * as Handlebars from "handlebars";
 import { logger } from "./log";
 import { ValidProjectName } from "./decorators";
 import { LennethCliError, ErrorCode } from "./error";
@@ -13,6 +18,7 @@ interface IProjectInfo {
   projectName?: string;
   projectVersion?: string;
   projectDescription?: string;
+  projectSource?: string;
 }
 
 export class LennethCliInit {
@@ -23,10 +29,12 @@ export class LennethCliInit {
   async createProjectName(projectName: string) {
     //创建目录
     try {
-      await this.downGitTemplate(projectName);
+      let projectSource = await this.downGitTemplate(projectName);
       // 获取交互数据
       let projectInfo = await this.inquirerShell(projectName);
-      await this.generatorTemplate(projectInfo);
+      await this.generatorTemplate(
+        Object.assign(projectInfo, { projectSource })
+      );
     } catch (e) {
       logger.error(e);
     }
@@ -37,17 +45,17 @@ export class LennethCliInit {
    * @param project 工程
    */
   async downGitTemplate(projectName: string) {
-    const templateUrl = "https://github.com/soraping/lenneth-demo.git";
-    const spinner = ora(`下载模版项目git地址 => ${templateUrl}`);
+    const templateUrl = "https://git.coding.net/soraping/lenneth-template.git";
+    const spinner = Ora(`下载模版项目git地址 => ${templateUrl}`);
     spinner.start();
     return new Promise((resolve, reject) => {
-      download({ source: templateUrl, destination: projectName })
+      Download({ source: templateUrl, destination: projectName })
         .then(() => {
           spinner.succeed();
           logger.success(
             `工程模版下载成功，路径 => ${path_resolve(projectName)}`
           );
-          resolve(1);
+          resolve(path_resolve(projectName));
         })
         .catch(e => {
           spinner.fail();
@@ -62,33 +70,76 @@ export class LennethCliInit {
    */
   async inquirerShell(projectName: string): Promise<IProjectInfo> {
     return new Promise((resolve, reject) => {
-      inquirer
-        .prompt([
-          {
-            name: "projectName",
-            message: "项目的名称",
-            default: projectName
-          },
-          {
-            name: "projectVersion",
-            message: "项目的版本号",
-            default: "0.0.1"
-          },
-          {
-            name: "projectDescription",
-            message: "项目的简介",
-            default: `A project named ${projectName}`
-          }
-        ])
-        .then(answers => {
-          resolve(answers);
-        });
+      Inquirer.prompt([
+        {
+          name: "projectName",
+          message: "项目的名称",
+          default: projectName
+        },
+        {
+          name: "projectVersion",
+          message: "项目的版本号",
+          default: "0.0.1"
+        },
+        {
+          name: "projectDescription",
+          message: "项目的简介",
+          default: `A project named ${projectName}`
+        }
+      ]).then(answers => {
+        resolve(answers);
+      });
     });
   }
 
   /**
-   * 生成模版
+   * 生成模版，替换关键字段
    * @param projectInfo
    */
-  async generatorTemplate(projectInfo: IProjectInfo) {}
+  async generatorTemplate(projectInfo: IProjectInfo) {
+    const metalsmith = Metalsmith(process.cwd())
+      .metadata(projectInfo)
+      .clean(false)
+      .source(projectInfo.projectSource)
+      .destination(projectInfo.projectSource);
+
+    // 获取ignore文件
+    const ignoreFile = path.join(__dirname, "templates.ignore");
+    if (fs.existsSync(ignoreFile)) {
+      metalsmith.use((files, metalsmith, done) => {
+        const meta = metalsmith.metadata();
+        const ignores = Handlebars.compile(
+          fs.readFileSync(ignoreFile).toString()
+        )(meta)
+          .split("\n")
+          .filter(item => !!item.length);
+        Object.keys(files).forEach(fileName => {
+          // 移除被忽略的文件
+          ignores.forEach(ignorePattern => {
+            if (Minimatch(fileName, ignorePattern)) {
+              delete files[fileName];
+            }
+          });
+        });
+        done();
+      });
+    }
+
+    metalsmith
+      .use((files, metalsmith, done) => {
+        const meta = metalsmith.metadata();
+        Object.keys(files).forEach(fileName => {
+          const t = files[fileName].contents.toString();
+          files[fileName].contents = new Buffer(Handlebars.compile(t)(meta));
+        });
+        done();
+      })
+      .build(err => {
+        if (err) {
+          logger.error(err);
+        } else {
+          logger.success(`工程 ${projectInfo.projectName} 新建完成`);
+        }
+      });
+  }
 }
